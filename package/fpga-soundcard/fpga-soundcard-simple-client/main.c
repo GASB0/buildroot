b859a1daa5e8a6e167e729a9f5603eecb62768b1
Gabriel Santos <santos.baez.gabriel.elo@gmail.com>
2024-06-10 20:47:04 -0400

    Getting to write values from bin_file to fpga through client

--- package/fpga-soundcard/fpga-soundcard-simple-client/main.c
+++ package/fpga-soundcard/fpga-soundcard-simple-client/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <strings.h>
+#include <stdlib.h>
 #include <alsa/asoundlib.h>
 
 
@@ -19,6 +20,70 @@ int lookup_id(snd_ctl_elem_id_t *id, snd_ctl_t *handle)
 	return 0;
 }
 
+int write_sample_coefficients_from_file(snd_ctl_t *handle, snd_ctl_elem_id_t *id,  snd_ctl_elem_value_t *value, const char* file_name) {
+	int err;
+	char* data = (char*)malloc(8*sizeof(char));
+	char read_byte;
+	int bytes_read;
+	FILE *fp;
+	uint32_t bram_addr = 0x00000000;
+
+	snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_CARD);
+	snd_ctl_elem_id_set_name(id, "FPGA FIR Coefficients Values");
+	if ((err = lookup_id(id, handle)) < 0){
+	    return err;
+	}
+
+	if ((fp = fopen(file_name,"r")) == NULL) {
+            fprintf(stderr, "Couldn't open the file you are requesting");
+	    return -1;
+	}
+
+	snd_ctl_elem_value_set_id(value, id);
+
+	// Verify by getting the ID from the value structure
+        fprintf(stderr, "Control element name set to: %s\n", snd_ctl_elem_id_get_name(id));
+        fprintf(stderr, "Control element numid set to: %u\n", snd_ctl_elem_id_get_numid(id));
+
+        fprintf(stderr, "Reading file coefficients file, this can take a while...\n");
+	while(1) {
+	  // Adding BRAM destination address for our read bytes
+	  data[0] = (bram_addr >> 0)  & 0xFF; // LSB
+	  data[1] = (bram_addr >> 8)  & 0xFF;
+	  data[2] = (bram_addr >> 16) & 0xFF;
+	  data[3] = (bram_addr >> 24) & 0xFF; // MSB
+
+	  // Actually reading the bytes from the file
+	  bytes_read=fread(&data[4], sizeof(char), 4, fp);
+	  if(bytes_read < 4) {
+	    if(feof(fp)) {
+	      if (bytes_read > 0) {
+		fprintf(stderr, "Something's wrong with your file, check your format!\n");
+		return -1;
+	      }
+	      fprintf(stderr, "Whole file succesffully read\n");
+	      break;
+	    } else {
+	      perror("Error reading from file");
+	      return -1;
+	    }
+	  }
+
+	  // Writing bytes to codec control
+	  snd_ctl_elem_set_bytes(value, data, sizeof(data));
+	  if ((err = snd_ctl_elem_write(handle, value)) < 0) {
+	    fprintf(stderr, "Control element write error: %s\n",
+		    snd_strerror(err));
+	    return err;
+	  }
+
+	  bram_addr++;
+	}
+
+	fclose(fp);
+	return 0;
+}
+
 int main(int argc, char *argv[]){
 	int err;
 	snd_ctl_t *handle;
@@ -32,44 +97,29 @@ int main(int argc, char *argv[]){
 		return err;
 	}
 
-	snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_CARD);
-	snd_ctl_elem_id_set_name(id, "FPGA FIR Coefficients Values");
-	if ((err = lookup_id(id, handle)) < 0){
-	    return err;
-	  }
-
-	snd_ctl_elem_value_set_id(value, id);
-
-	// Verify by getting the ID from the value structure
-        fprintf(stderr, "Control element name set to: %s\n", snd_ctl_elem_id_get_name(id));
-        fprintf(stderr, "Control element numid set to: %u\n", snd_ctl_elem_id_get_numid(id));
-	
-	unsigned char data[8] = { 0x00, 0x00, 0x00, 0x00, 0x0A, 0x0B, 0x0C, 0x0D};
-        snd_ctl_elem_set_bytes(value, data, sizeof(data));
-
-	// Writing test section:
-	if ((err = snd_ctl_elem_write(handle, value)) < 0) {
-	  fprintf(stderr, "Control element write error: %s\n",
-	  snd_strerror(err));
-	  return err;
+	if (write_sample_coefficients_from_file(handle, id, value, "./filter_file.bin")!=0) {
+	  fprintf(stderr, "The program terminated unexpectedly");
+	  return -1;
 	}
 
 	// Reading test section:
-	if ((err = snd_ctl_elem_read(handle, value)) < 0) {
-	  fprintf(stderr, "Control element read error: %s\n",
-	  snd_strerror(err));
-	  return err;
-	}
-	
-        unsigned char* reading_result = malloc(512*sizeof(unsigned char));
+	/* unsigned char data[8] = { 0x00, 0x00, 0x00, 0x00, 0x0A, 0x0B, 0x0C, 0x0D}; */
+        /* snd_ctl_elem_set_bytes(value, data, sizeof(data)); */
 
-	memcpy(reading_result, snd_ctl_elem_value_get_bytes(value), 512);
-	printf("%c", *(reading_result+0));
+	/* if ((err = snd_ctl_elem_read(handle, value)) < 0) { */
+	/*   fprintf(stderr, "Control element read error: %s\n", */
+	/*   snd_strerror(err)); */
+	/*   return err; */
+	/* } */
+	
+        /* unsigned char* reading_result = malloc(512*sizeof(unsigned char)); */
+
+	/* memcpy(reading_result, snd_ctl_elem_value_get_bytes(value), 512); */
+	/* printf("%c", *(reading_result+0)); */
 
 	// Free allocated memory
 	snd_ctl_elem_id_clear(id);
 	snd_ctl_elem_value_clear(value);
-	free(reading_result);
 
 	// Close the control interface
 	snd_ctl_close(handle);
