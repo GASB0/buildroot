c9cdaa48baa7a93cd7b9e5bcdd075e5343b5c896
Gabriel Santos <santos.baez.gabriel.elo@gmail.com>
2024-06-16 17:46:10 -0400

    Starting the uart mmio lab

---
+++ package/custom-uart/custom-uart.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# Custom uart module
+#
+################################################################################
+
+CUSTOM_UART_VERSION = 0.0.1
+CUSTOM_UART_SITE = package/custom-uart
+CUSTOM_UART_SITE_METHOD = local
+CUSTOM_UART_DEPENDENCIES =
+
+# Kernel module section
+CUSTOM_UART_MODULE_SUBDIRS = serial
+CUSTOM_UART_MODULE_MAKE_OPTS = \
+        INSTALL_MOD_DIR=kernel/drivers/misc \
+        CONFIG_CUSTOM_UART=m \
+        CONFIG_CUSTOM_UART_DEBUG=y
+
+define CUSTOM_UART_INSTALL_MOD
+        # $(INSTALL) -D -m 644 $(@D)/$(CUSTOM_UART_MODULE_SUBDIRS)/fpga-soundcard.dtbo $(BUILD_DIR)/rpi-firmware-$(RPI_FIRMWARE_VERSION)/boot/overlays/
+
+        # scp -rpO $(BUILD_DIR)/fpga-soundcard-$(CUSTOM_UART_VERSION)/module-fpga-soundcard/snd-soc-fpga-codec.ko root@buildroot.local:/root/
+endef
+CUSTOM_UART_POST_INSTALL_TARGET_HOOKS += CUSTOM_UART_INSTALL_MOD
+
+$(eval $(kernel-module))
---
+++ package/custom-uart/serial/serial.c
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/init.h>
+#include <linux/module.h>
+#include <asm/io.h>
+#include <linux/platform_device.h>
+
+/* Add your code here */
+struct serial_dev {
+        void __iomem *regs;
+};
+
+static u32 reg_read(struct serial_dev *serial, unsigned int reg) {
+  return readl(serial->regs+4*reg);
+}
+
+static int reg_write(struct serial_dev *serial, u32 val, unsigned int reg) {
+  return writel(val, serial->regs+4*reg);
+}
+
+static int serial_probe(struct platform_device *pdev)
+{
+        struct serial_dev *serial;
+	serial = devm_kzalloc(&pdev->dev, sizeof(*serial), GFP_KERNEL);
+	if (!serial)
+	        return -ENOMEM;
+
+	serial->regs = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(serial->regs))
+	        return PTR_ERR(serial->regs);
+	
+	pm_runtime_enable(&pdev->dev);
+	pm_runtime_get_sync(&pdev->dev);
+
+	
+	pr_info("Called %s\n", __func__);
+	return 0;
+}
+
+static int serial_remove(struct platform_device *pdev)
+{
+        pm_runtime_disable(&pdev->dev);
+
+	pr_info("Called %s\n", __func__);
+        return 0;
+}
+
+static const struct of_device_id custom_uart_of_match[] = {
+	{ .compatible = "custom,uart", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, custom_uart_of_match);
+
+static struct platform_driver serial_driver = {
+        .driver = {
+                .name = "serial",
+                .owner = THIS_MODULE,
+		.of_match_table = custom_uart_of_match,
+        },
+        .probe = serial_probe,
+        .remove = serial_remove,
+};
+module_platform_driver(serial_driver);
+
+MODULE_LICENSE("GPL");
