f54cc39e66cb99b20321782c69765007a26dc3e7
Gabriel Santos <santos.baez.gabriel.elo@gmail.com>
2024-06-17 20:02:15 -0400

    Fixing dts

--- package/custom-uart/custom-uart.mk
+++ package/custom-uart/custom-uart.mk
@@ -8,6 +8,7 @@ CUSTOM_UART_VERSION = 0.0.1
 CUSTOM_UART_SITE = package/custom-uart
 CUSTOM_UART_SITE_METHOD = local
 CUSTOM_UART_DEPENDENCIES =
+CUSTOM_UART_INSTALL_IMAGES = YES
 
 # Kernel module section
 CUSTOM_UART_MODULE_SUBDIRS = serial
@@ -16,12 +17,13 @@ CUSTOM_UART_MODULE_MAKE_OPTS = \
         CONFIG_CUSTOM_UART=m \
         CONFIG_CUSTOM_UART_DEBUG=y
 
-define CUSTOM_UART_INSTALL_MOD
-        # $(INSTALL) -D -m 644 $(@D)/$(CUSTOM_UART_MODULE_SUBDIRS)/fpga-soundcard.dtbo $(BUILD_DIR)/rpi-firmware-$(RPI_FIRMWARE_VERSION)/boot/overlays/
-
-        # scp -rpO $(BUILD_DIR)/fpga-soundcard-$(CUSTOM_UART_VERSION)/module-fpga-soundcard/snd-soc-fpga-codec.ko root@buildroot.local:/root/
+define CUSTOM_UART_INSTALL_OVERLAYS
+        $(INSTALL) -D -m 0644 $(@D)/serial/custom-uart.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
 endef
-CUSTOM_UART_POST_INSTALL_TARGET_HOOKS += CUSTOM_UART_INSTALL_MOD
+
+define CUSTOM_UART_INSTALL_IMAGES_CMDS
+        $(CUSTOM_UART_INSTALL_OVERLAYS)
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
--- package/custom-uart/serial/serial.c
+++ package/custom-uart/serial/serial.c
@@ -6,23 +6,38 @@
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 
+// Define offsets for UART registers
+#define UART2_DR    0x00
+#define UART2_FR    0x18
+#define UART2_IBRD  0x24
+#define UART2_FBRD  0x28
+#define UART2_LCRH  0x2C
+#define UART2_CR    0x30
+#define UART2_IMSC  0x38
+#define UART2_ICR   0x44
+
+
 /* Add your code here */
 struct serial_dev {
         void __iomem *regs;
 };
 
 static u32 reg_read(struct serial_dev *serial, unsigned int reg) {
-        return readl(serial->regs+4*reg);
+        return readl(serial->regs+reg);
 }
 
 static int reg_write(struct serial_dev *serial, u32 val, unsigned int reg) {
-        writel(val, serial->regs+4*reg);
+        writel(val, serial->regs+reg);
         return 0;
 }
 
 static int serial_probe(struct platform_device *pdev)
 {
         struct serial_dev *serial;
+	u32 uartclk;
+	int ret, baud_divisor;
+	pr_info("Called %s\n", __func__);
+
 	serial = devm_kzalloc(&pdev->dev, sizeof(*serial), GFP_KERNEL);
 	if (!serial)
 	        return -ENOMEM;
@@ -34,8 +49,23 @@ static int serial_probe(struct platform_device *pdev)
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_get_sync(&pdev->dev);
 
+	ret = of_property_read_u32(pdev->dev.of_node, "clock-frequency", &uartclk);
+	if(ret) {
+	        dev_err(&pdev->dev, "clock-frequency property not found in Device Tree\n");
+	        return ret;
+	}
+
+	printk(KERN_INFO"The read uartclk is: %lu\n", (unsigned long)uartclk);
+	baud_divisor = (int)uartclk/16/115200;
 	
-	pr_info("Called %s\n", __func__);
+	reg_write(serial, UART2_CR, 0x00); // Disabling UART
+	reg_write(serial, UART2_LCRH, (1 << 4) | (1 << 5) | (1 << 6)); // Flushing transmit FIFO and setting parity
+
+	reg_write(serial, UART2_CR, (1 << 8)); // Configuring transmision of data
+	reg_write(serial, UART2_DR, 0xAA); // Writing a value into data register
+
+	reg_write(serial, UART2_CR, (1 << 8) | (1 << 0)); // Enabling UART
+
 	return 0;
 }
 
@@ -48,7 +78,7 @@ static int serial_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id custom_uart_of_match[] = {
-	{ .compatible = "custom,uart", },
+	{ .compatible = "bootlin,serial", },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, custom_uart_of_match);
